dd9f0a131917044ff1ae6f1d3346e182c4d76586
lewyk <lewyk@hotmail.com>
2020-11-11 00:17:23 +0200

    add 3 homeworks

---
+++ task2/task2.js
@@ -0,0 +1,32 @@
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+// В JS есть массив строк.
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+// Напиши скрипт, который для каждого элемента массива
+// ingredients создаст отдельный li,   
+// после чего вставит все
+// li за одну операцию в список ul.ingredients.Для создания
+// DOM - узлов используй document.createElement().
+
+const ingredientsList = document.querySelector('ul#ingredients');
+
+const makeList = array => {
+  return array.map((el) => {
+    const liRef = document.createElement('li');
+    liRef.textContent = el;
+    return liRef
+  })
+}
+
+ingredientsList.append(...makeList(ingredients))
